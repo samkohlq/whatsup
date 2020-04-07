4ed084b2e095ca458f6a9b303f0468ef3abe060f
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-04-07 20:06:09 +0800

    created sign-in function

--- lib/sign_in_with_google.dart
+++ lib/sign_in_with_google.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:whatsup/main.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
@@ -32,46 +35,46 @@ class SignInButton extends StatelessWidget {
       onPressed: () async {
         await signInWithGoogle();
         print("!!!!!!!!!!!!!!!!! AFTER SIGN IN !!!!!!!!!!!!!!!!!!!");
-        // Navigator.push(
-        //   context,
-        //   MaterialPageRoute(builder: (context) => MainContent()),
-        // );
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => MainContent()),
+        );
       },
     );
   }
 }
 
 Future signInWithGoogle() async {
-  // try {
-  final GoogleSignInAccount googleSignInAccount =
-      await googleSignIn.signIn().catchError((onError) {
-    print('Error: $onError');
-  });
-  // if (googleSignInAccount != null) {
-  // final googleAuth = await googleSignInAccount.authentication;
-  // final AuthCredential credential = GoogleAuthProvider.getCredential(
-  //     idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
-  // final authResult = await _auth.signInWithCredential(credential);
-  // final FirebaseUser user = authResult.user;
+  try {
+    final GoogleSignInAccount googleSignInAccount =
+        await googleSignIn.signIn().catchError((onError) {
+      print('Error: $onError');
+    });
+    if (googleSignInAccount != null) {
+      final googleAuth = await googleSignInAccount.authentication;
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+      final authResult = await _auth.signInWithCredential(credential);
+      final FirebaseUser user = authResult.user;
 
-  //     // confirms that user is signed in
-  // assert(!user.isAnonymous);
-  //     assert(await user.getIdToken() != null);
+      // confirms that user is signed in
+      assert(!user.isAnonymous);
+      assert(await user.getIdToken() != null);
 
-  //     final FirebaseUser currentUser = await _auth.currentUser();
-  //     assert(user.uid == currentUser.uid);
+      final FirebaseUser currentUser = await _auth.currentUser();
+      assert(user.uid == currentUser.uid);
 
-  //     print(user.uid);
-  //     Firestore.instance.document('users/${user.uid}').setData({
-  //       'email': user.email,
-  //       'name': user.displayName,
-  //     });
-  //     return currentUser;
-  // }
-  //   return null;
-  // } on PlatformException catch (error) {
-  //   print('Error: $error');
-  // }
+      print(user.uid);
+      Firestore.instance.document('users/${user.uid}').setData({
+        'email': user.email,
+        'name': user.displayName,
+      });
+      return currentUser;
+    }
+    return null;
+  } on PlatformException catch (error) {
+    print('Error: $error');
+  }
 }
 
 void signOut() async {
